ac25fe5951369fd153cb7a9411bfd98755fd1ec5
alfonsobries <alfonsobries@gmail.com>
2019-02-23 13:33:01 -0600

    Test that all the user info is being saved

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -30,4 +30,23 @@ class RegisterTest extends TestCase
         
         $this->assertTrue(Hash::check('secret', User::latest()->first()->password));
     }
+
+    /** @test */
+    public function it_store_all_the_fillable_user_data()
+    {
+        $userData = [
+            'name' => 'Alfonso Bribiesca',
+            'username' => 'alfonsobries',
+            'email' => 'alfonso@vexilo.com',
+            'website' => 'https://www.vexilo.com',
+            'bio' => 'Full stack developer',
+            'phone' => '+52 55 555555555',
+            'gender' => User::GENDER_MALE,
+            'is_private' => true,
+        ];
+
+        $this->postJson(route('register'), array_merge($userData, ['password' => 'secret', 'password_confirmation' => 'secret']));
+        
+        $this->assertDatabaseHas('users', $userData);
+    }
 }
